f30975563ab915cc7117c583e2475261fe2455c9
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-03-16 17:40:45 -0300

    only missing deleteBook func

--- router/book.go
+++ router/book.go
@@ -46,3 +46,72 @@
 
    return c.Status(200).JSON(fiber.Map{"data": books})
  }
+
+ type createDTO struct {
+   Title string `json:"title" bson:"title"`
+   Author string `json:"author" bson:"author"`
+   Year string `json:"year" bson:"year"`
+ }
+
+ func createBook(c *fiber.Ctx) error {
+   //validate the body
+   b := new(createDTO)
+
+   if err := c.BodyParser(b); err != nil {
+     return c.Status(400).JSON(fiber.Map{
+       "error": "Failed to create book",
+       "message": err.Error(),
+     })
+   }
+
+   // return the book
+   return c.Status(201).JSON(fiber.Map{
+     "result": result,
+   })
+ }
+
+ type updateDTO struct {
+  Title  string `json:"title,omitempty" bson:"title,omitempty"`
+	Author string `json:"author,omitempty" bson:"author,omitempty"`
+	Year   string `json:"year,omitempty" bson:"year,omitempty"`
+ }
+
+ func updateBook(c *fiber.Ctx) error {
+   // validate the body
+   b := new(updateDTO)
+   if err := c.BodyParser(b); err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"error": "Invalid body",
+		})
+	}
+
+  //get the id 
+  id := c.Param("id")
+  if id == "" {
+		return c.Status(400).JSON(fiber.Map{
+			"error": "id is required",
+		})
+	}
+
+  objectId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"error": "invalid id",
+		})
+	}
+
+  // update the book
+	coll := common.GetDBCollection("books")
+	result, err := coll.UpdateOne(c.Context(), bson.M{"_id": objectId}, bson.M{"$set": b})
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"error":   "Failed to update book",
+			"message": err.Error(),
+		})
+	}
+
+	// return the book
+	return c.Status(200).JSON(fiber.Map{
+		"result": result,
+	})
+ }
